13cc1813c626ee55ebfccface7d81be47c6f3bb9
Nicola Righetti <nicolarighetti79@gmail.com>
2020-07-01 20:36:42 +0200

    dfm_trim

--- R/reinert.R
+++ R/reinert.R
@@ -5,6 +5,8 @@
 #' @param df a data frame with at least a column with textual data and a column with documents' ID
 #' @param docid_field name of the column (in quotation marks) containing the IDs of the documents (default NULL)
 #' @param text_field  name of the column (in quotation marks) containing textual data
+#' @param min_docfreq minimum values of a feature's document frequency, below which features will be removed (default 0.5 percentile)
+#' @param max_docfreq maximum values of a feature's document frequency, above which features will be removed (default 99 percentile)
 #' @param k.max desired maximum number of clusters (default NULL).
 #' @param min_uc_size minimum number of forms by document (default 10).
 #' @param min_split_members don't try to split groups with fewer members (default 5).
@@ -22,7 +24,7 @@
 #' reinert_clustering <- reinert(df, docid_field = "URL", text_field = "Message", k = 10)
 #' rainette::rainette_explor(reinert_clustering[[1]], reinert_clustering[[2]])}
 #'
-#' @importFrom quanteda dfm corpus dfm_trim dfm_tfidf rowSums
+#' @importFrom quanteda dfm corpus dfm_trim dfm_tfidf dfm_trim
 #' @importFrom rainette rainette
 #'
 #' @export
@@ -31,6 +33,8 @@ reinert <-
   function(df,
            docid_field = NULL,
            text_field = NULL,
+           min_docfreq = 0.5, 
+           max_docfreq = 99,
            segment_size = 40,
            k = NULL,
            min_uc_size = 10,
@@ -48,7 +52,7 @@ reinert <-
     mydfm <-
       quanteda::dfm(mycorpus)
     
-   # mydfm <- mydfm[quanteda::rowSums(mydfm) != 0,]
+    mydfm <- quanteda::dfm_trim(mydfm, min_docfreq = min_docfreq, max_docfreq = max_docfreq, termfreq_type = "quantile")
     
     res <-
       rainette::rainette(
